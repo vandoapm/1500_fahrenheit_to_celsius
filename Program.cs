199367558528099dc1513f757585ef44a63770fe
VANDO MONTEIRO <vando.monteiro@etec.sp.gov.br>
2022-03-18 11:12:08 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1500_fahrenheit_to_celsius
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Converter temperatura medida em graus Fahrenheit para Celsius");
+
+            Console.WriteLine();
+
+            Console.Write("Digite a temperatura em ºF: ");
+            float f = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("A temperatura de " + f + "ºF é igual a " + ((f - 32) * 5)/9 + "ºC");
+
+            Console.ReadLine();
+
+        }
+    }
+}
